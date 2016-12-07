14acdb158bf85582e6b162ed0d8aea02f6b4c6eb
twobit <s8572327@gmail.com>
2016-12-07 11:41:48 +0100

    dry

--- 2016/06/p.py
+++ 2016/06/p.py
@@ -1,2 +1,1 @@
-print "".join(max(l, key=l.count) for l in zip(*file("in")))
-print "".join(min(l, key=l.count) for l in zip(*file("in")))
+for m in max, min: print "".join(m(l, key=l.count) for l in zip(*file("in")))
