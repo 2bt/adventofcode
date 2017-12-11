f35ff8571af1f6bfd65b2cba342754824596bb27
2bt <s8572327@gmail.com>
2017-12-11 10:55:20 +0100

    11

---
+++ 2017/11/p.py
@@ -0,0 +1,16 @@
+p = 0, 0, 0
+l = 0
+d = {
+	"n":  ( 0, 1,-1),
+	"s":  ( 0,-1, 1),
+	"ne": ( 1, 0,-1),
+	"sw": (-1, 0, 1),
+	"nw": ( 1,-1, 0),
+	"se": (-1, 1, 0),
+}
+for w in open("input").read().strip().split(","):
+	m = d[w]
+	p = (p[0] + m[0], p[1] + m[1], p[2] + m[2])
+	l = max(map(abs, p) + [l])
+print max(map(abs, p))
+print l
