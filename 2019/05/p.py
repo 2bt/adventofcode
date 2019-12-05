e48b941e5cb21e8136e6fcbb54f0a769324eb9ad
2bt <s8572327@gmail.com>
2019-12-05 10:46:57 +0100

    05

---
+++ 2019/05/p.py
@@ -0,0 +1,42 @@
+
+PARAM_COUNT = {
+    1: 3,
+    2: 3,
+    3: 1,
+    4: 1,
+    5: 2,
+    6: 2,
+    7: 3,
+    8: 3,
+    99: 0,
+}
+
+def f(mem, id):
+    pc = 0
+    while 1:
+        op = mem[pc] % 100
+        mode = mem[pc] / 100
+        pc += 1
+        p = []
+        for _ in range(PARAM_COUNT[op]):
+            x = pc
+            pc += 1
+            if mode % 10 == 0: x = mem[x]
+            mode /= 10
+            p.append(x)
+
+        if   op == 1: mem[p[2]] = mem[p[0]] + mem[p[1]]
+        elif op == 2: mem[p[2]] = mem[p[0]] * mem[p[1]]
+        elif op == 3: mem[p[0]] = id
+        elif op == 4: print mem[p[0]]
+        elif op == 5:
+            if mem[p[0]] != 0: pc = mem[p[1]]
+        elif op == 6:
+            if mem[p[0]] == 0: pc = mem[p[1]]
+        elif op == 7: mem[p[2]] = mem[p[0]] < mem[p[1]]
+        elif op == 8: mem[p[2]] = mem[p[0]] == mem[p[1]]
+        elif op == 99: break
+
+q = list(eval(open("input").read()))
+f(q * 1, 1)
+f(q * 1, 5)
