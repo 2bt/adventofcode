8f9619f8f7387a239f5c8dcdb3c4cbd4e6e8b7c4
2bt <s8572327@gmail.com>
2019-12-18 17:00:02 +0100

    16

--- 2019/16/p.py
+++ 2019/16/p.py
@@ -1,8 +1,32 @@
-a = map(int, open("input").read().strip())
+A = map(int, open("input").read().strip())
+a = A * 1
+b = [0] * len(a)
 for _ in range(100):
-    b = []
-    for i in range(len(a)):
-        q = abs(sum(x * [0, 1, 0, -1][((j + 1) / (1 + i))%4] for j, x in enumerate(a))) % 10
-        b.append(q)
-    a = b
+    s = [0]
+    for x in a: s.append(s[-1] + x)
+    for n in xrange(0, len(a)):
+        i = n
+        c = 0
+        q = 1
+        while i < len(a):
+            m = min(len(a), i + n + 1)
+            c += (s[m] - s[i]) * q
+            i += (n + 1) * 2
+            q *= -1
+        b[n] = abs(c) % 10
+    a, b = b, a
 print "".join(map(str, a[:8]))
+
+
+a = A * 10000
+b = [0] * len(a)
+o = 0
+for i, x in enumerate(a[:7]): o = o * 10 + x
+assert o > len(a) / 2
+for _ in range(100):
+    c = 0
+    for i in range(len(a) - 1, o - 1, -1):
+        b[i] = c = (c + a[i]) % 10
+    a, b = b, a
+
+print "".join(map(str, a[o:o + 8]))
