1adcf211c55958733fe8f0ecd2df47719d8096df
2bt <s8572327@gmail.com>
2020-12-10 16:26:38 +0100

    shorten

--- 2020/10/p.py
+++ 2020/10/p.py
@@ -5,9 +5,7 @@ for x, y in zip(a, a[1:]): c[y - x] += 1
 print(c[1] * c[3])
 
 
-def f(i, cache={len(a) - 1: 1}):
-    if i in cache: return cache[i]
-    c = sum(f(i + o) for o in [1, 2, 3] if i + o < len(a) and a[i + o] <= a[i] + 3)
-    cache[i] = c
-    return c
+def f(i, c={len(a) - 1: 1}):
+    if i not in c: c[i] = sum(f(i + o) for o in [1, 2, 3] if i + o < len(a) and a[i + o] <= a[i] + 3)
+    return c[i]
 print(f(0))
