406839468fea0c5dadcb004dff872e329d2ebc2f
2bt <s8572327@gmail.com>
2021-12-11 19:51:54 +0100

    11

---
+++ 2021/11/p.py
@@ -0,0 +1,15 @@
+a = [-1 if x == "\n" else int(x) for x in open("input").read()] + [-1] * 11
+def f(i):
+    if a[i] == -1: return
+    a[i] += 1
+    if a[i] == 10:
+        for o in [-12, -11, -10, -1, 1, 10, 11, 12]: f(i + o)
+c = n = q = 0
+while q < 100:
+    for i in range(len(a)): f(i)
+    q = sum(x > 9 for x in a)
+    if n < 100: c += q
+    a = [[x, 0][x > 9] for x in a]
+    n += 1
+print(c)
+print(n)
