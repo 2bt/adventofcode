0e71dca6eda1a522c5e79e83a91c6bb1a4c646c3
2bt <s8572327@gmail.com>
2021-12-12 15:43:01 +0100

    x

--- 2021/12/p.py
+++ 2021/12/p.py
@@ -1,19 +1,18 @@
 from collections import defaultdict
 
 d = defaultdict(set)
-
 for l in open("input"):
     a, b = l.strip().split("-")
     d[a].add(b)
     d[b].add(a)
 
-def f(n, k, v=set()):
+def f(k, n="start", v=set()):
     if n in v:
         if n == "start" or k: return 0
         else: k = True
     if n == "end": return 1
     if n.islower(): v = v | {n}
-    return sum(f(x, k, v) for x in d[n])
+    return sum(f(k, x, v) for x in d[n])
 
-print(f("start", True))
-print(f("start", False))
+print(f(True))
+print(f(False))
