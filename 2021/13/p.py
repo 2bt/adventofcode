e16680b197f04dc78d5e384f50749ddfef828e0e
2bt <s8572327@gmail.com>
2021-12-13 15:26:04 +0100

    x

--- 2021/13/p.py
+++ 2021/13/p.py
@@ -4,11 +4,11 @@ for l in f:
     if not l.strip(): break
     p.add(tuple(map(int, l.split(","))))
 
-for l in f:
+for i, l in enumerate(f):
     v = int(l[13:])
     p = {(2 * v - x if l[11] == "x" and x > v else x,
           2 * v - y if l[11] == "y" and y > v else y) for x, y in p}
-    print(len(p))
+    if i == 0: print(len(p))
 
 mx, my = map(max, zip(*p))
 for y in range(my + 1): print("".join(" #"[(x, y) in p] for x in range(mx + 1)))
