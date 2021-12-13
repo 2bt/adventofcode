4ea9a949033f4fa8a34dc47173d1c836682ea4ea
2bt <s8572327@gmail.com>
2021-12-13 15:22:02 +0100

    13

---
+++ 2021/13/p.py
@@ -0,0 +1,14 @@
+f = open("input")
+p = set()
+for l in f:
+    if not l.strip(): break
+    p.add(tuple(map(int, l.split(","))))
+
+for l in f:
+    v = int(l[13:])
+    p = {(2 * v - x if l[11] == "x" and x > v else x,
+          2 * v - y if l[11] == "y" and y > v else y) for x, y in p}
+    print(len(p))
+
+mx, my = map(max, zip(*p))
+for y in range(my + 1): print("".join(" #"[(x, y) in p] for x in range(mx + 1)))
