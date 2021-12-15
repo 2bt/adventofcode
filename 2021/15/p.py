6b7bfca94e6277cfbf62217806a3e591762e2ba5
2bt <s8572327@gmail.com>
2021-12-15 18:15:29 +0100

    x

--- 2021/15/p.py
+++ 2021/15/p.py
@@ -3,24 +3,18 @@ import queue
 a = [list(map(int, l.strip())) for l in open("input")]
 w = len(a)
 
-def f(x, y):
-    if x < 0 or y < 0: return 0
-    if x >= w * n or y >= w * n: return 0
-    return (a[y % w][x % w] - 1 + x // w + y // w) % 9 + 1
-
 for n in 1, 5:
     q = queue.PriorityQueue()
     q.put((0, 0, 0))
     v = {(0, 0)}
-    t = w * n - 1
     while q:
         r, x, y = q.get()
-        if (x, y) == (t, t): break
+        if x == y == w * n - 1: break
         for i in range(4):
             x2 = x + [-1, 0, 1, 0][i]
             y2 = y + [0, -1, 0, 1][i]
-            dr = f(x2, y2)
-            if (x2, y2) in v or not dr: continue
+            if (x2, y2) in v or x2 < 0 or y2 < 0 or x2 >= w * n or y2 >= w * n: continue
+            r2 = (a[y2 % w][x2 % w] - 1 + x2 // w + y2 // w) % 9 + 1 + r
             v.add((x2, y2))
-            q.put((r + dr, x2, y2))
+            q.put((r2, x2, y2))
     print(r)
