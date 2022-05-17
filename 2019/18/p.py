43e0f8b40d7c4071427bd71c33fcbbe4f49c4c43
2bt <s8572327@gmail.com>
2022-05-17 13:32:41 +0200

    18.2

--- 2019/18/p.py
+++ 2019/18/p.py
@@ -1,57 +1,56 @@
-import sys
-sys.setrecursionlimit(100000)
+import string
+from queue import PriorityQueue
 
+UPPER = set(map(ord, string.ascii_uppercase))
+LOWER = set(map(ord, string.ascii_lowercase))
+WALL  = ord("#")
+START = ord("@")
 
-g = open("input").read()
-l = g.find("\n") + 1
-keys = "".join(sorted(c for c in g if c.isalpha() and c.islower()))
+g = bytearray(open("input", "rb").read())
+L = g.find(10) + 1
+keys = sum(1 << (c - 97) for c in g if c in LOWER)
 
-visited = set()
-queue = [(g.index("@"), "")]
-for w in xrange(9999):
-    queue2 = []
-    for p, k in queue:
-        for q in p+l, p-l, p-1, p+1:
-            if g[q] == "#": continue
-            if g[q].isupper() and g[q].lower() not in k: continue
-            c = k
-            if g[q].islower() and g[q] not in c:
-                c = "".join(sorted(c + g[q]))
-                if c == keys: break
-            if (q, c) in visited: continue
-            visited.add((q, c))
-            queue2.append((q, c))
-        if c == keys: break
-    queue = queue2
-    if c == keys: break
-print w + 1
+def reachable_keys(p, k):
+    queue = [p]
+    visited = set(queue)
+    for w in range(1, 9999):
+        queue2 = queue
+        queue = []
+        for p in queue2:
+            for q in p+L, p-L, p-1, p+1:
+                if q in visited: continue
+                c = g[q]
+                if c == WALL: continue
+                if c in UPPER and ((1 << (c - 65)) & k) == 0: continue
+                if c in LOWER and ((1 << (c - 97)) & k) == 0:
+                    yield q, w
+                    continue
+                visited.add(q)
+                queue.append(q)
 
+for n in 0, 1:
+    queue = PriorityQueue()
+    visited = {}
 
-#if 0:
-#    p = g.index("@")
-#    g = list(g)
-#    g[p] = g[p-1] = g[p+1] = g[p-l] = g[p+l] = "#"
-#    g[p-1-l] = g[p-1+l] = g[p+1-l] = g[p+1+l] = "@"
-#    visited = set()
-#    queue = [((p-1-l, p-1+l, p+1-l, p+1+l), "")]
-#    for w in xrange(9999):
-#        queue2 = []
-#        for ps, k in queue:
-#            for i, p in enumerate(ps):
-#                for q in p+l, p-l, p-1, p+1:
-#                    if g[q] == "#": continue
-#                    if g[q].isupper() and g[q].lower() not in k: continue
-#                    c = k
-#                    if g[q].islower() and g[q] not in c:
-#                        print ps, k, w
-#                        c = "".join(sorted(c + g[q]))
-#                        if c == keys: break
-#
-#                    qs = ps[:i] + (q,) + ps[i + 1:]
-#                    if (qs, c) in visited: continue
-#                    visited.add((qs, c))
-#                    queue2.append((qs, c))
-#
-#        queue = queue2
-#        if c == keys: break
-#    print w + 1
+    if n:
+        p = g.find(b".@.")
+        g[p-L:p-L+3] = b"@#@"
+        g[p  :p  +3] = b"###"
+        g[p+L:p+L+3] = b"@#@"
+        queue.put((0, (p-L, p-L+2, p+L, p+L+2), 0))
+    else:
+        queue.put((0, (g.find(b"@"),), 0))
+
+    while not queue.empty():
+        w, ps, ks = queue.get()
+        if visited.get((ps, ks), w) < w: continue
+        if ks == keys:
+            print(w)
+            break
+        for i, p in enumerate(ps):
+            for q, v in reachable_keys(p, ks):
+                kk = ks | (1 << (g[q] - 97))
+                pp = ps[:i] + (q,) + ps[i+1:]
+                if (pp, kk) not in visited or visited[pp, kk] > w + v:
+                    queue.put((w + v, pp, kk))
+                    visited[pp, kk] = min(w + v, visited.get((pp, kk), w + v))
